4bd8222ea7944d5781c91a9c5d5c6d352964780c
Jorge Calero <jorge.calero@gmail.com>
2019-08-12 14:28:02 +0200

    works properly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question.include?('?')
+      return @answer = 'Silly question, get dressed and go to work!'
+    end
+
+    case @question
+    when 'I am going to work'
+      return @answer = 'Great!'
+    else
+      return @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
